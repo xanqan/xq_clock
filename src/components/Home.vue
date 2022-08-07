3de1999f10a0f73cf615d4e8e7a8bd896ec2b904
xanqan <2896106853@qq.com>
2022-08-07 20:39:04 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">首页</div>
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
+</template>
+
+<script lang="ts">
+// import { defineComponent, reactive ,ref} from 'vue'
+export default {
+  name: "App",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
