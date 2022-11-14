cd0fa02b56e8d716153fca2c4de51db23807f62b
xanqan <2896106853@qq.com>
2022-11-14 13:15:06 +0800

    feat: add a-img

--- components.d.ts
+++ components.d.ts
@@ -15,6 +15,7 @@ declare module '@vue/runtime-core' {
     ADropdown: typeof import('ant-design-vue/es')['Dropdown']
     AForm: typeof import('ant-design-vue/es')['Form']
     AFormItem: typeof import('ant-design-vue/es')['FormItem']
+    AImage: typeof import('ant-design-vue/es')['Image']
     AInput: typeof import('ant-design-vue/es')['Input']
     AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
     ALayout: typeof import('ant-design-vue/es')['Layout']
